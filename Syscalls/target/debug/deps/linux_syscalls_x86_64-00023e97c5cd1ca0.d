5fec1bb7eb214bf52f743ff8878946663f40e708
Srinivasa R <96676964+Wolfram70@users.noreply.github.com>
2023-05-13 01:30:29 +0530

    Delete Syscalls/target directory

--- Syscalls/target/debug/deps/linux_syscalls_x86_64-00023e97c5cd1ca0.d
+++
@@ -1,5 +0,0 @@
-/home/wolfram/School/IT253 OS/Project/RUST-API/Syscalls/target/debug/deps/linux_syscalls_x86_64-00023e97c5cd1ca0.rmeta: src/main.rs
-
-/home/wolfram/School/IT253 OS/Project/RUST-API/Syscalls/target/debug/deps/linux_syscalls_x86_64-00023e97c5cd1ca0.d: src/main.rs
-
-src/main.rs:
--- Syscalls/target/debug/deps/linux_syscalls_x86_64-0f990fff159e255f.d
+++
@@ -1,5 +0,0 @@
-/home/wolfram/School/IT253 OS/Project/RUST-API/Syscalls/target/debug/deps/linux_syscalls_x86_64-0f990fff159e255f.rmeta: src/main.rs
-
-/home/wolfram/School/IT253 OS/Project/RUST-API/Syscalls/target/debug/deps/linux_syscalls_x86_64-0f990fff159e255f.d: src/main.rs
-
-src/main.rs:
