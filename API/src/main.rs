396a9fafc43ba621a93c905a2718e13523720cf6
Srinivasa R <srinivasa.r0503@gmail.com>
2023-05-13 01:28:56 +0530

    cleaned up

---
+++ API/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let string = "Hello, world!\n";
+    syscalls::sys_write(1, string.as_ptr(), string.len() as u64);
+}
--- Syscalls/target/package/linux_syscalls_x86_64-0.1.0/target/debug/deps/linux_syscalls_x86_64-b9092d4ce698433e.d
+++
@@ -1,5 +0,0 @@
-/home/wolfram/School/IT253 OS/Project/RUST-API/Syscalls/target/package/linux_syscalls_x86_64-0.1.0/target/debug/deps/linux_syscalls_x86_64-b9092d4ce698433e: src/main.rs
-
-/home/wolfram/School/IT253 OS/Project/RUST-API/Syscalls/target/package/linux_syscalls_x86_64-0.1.0/target/debug/deps/linux_syscalls_x86_64-b9092d4ce698433e.d: src/main.rs
-
-src/main.rs:
