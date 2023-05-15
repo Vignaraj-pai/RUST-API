b0efe1aefe1dee1a527356ad2419b7c37bf50af3
Srinivasa R <srinivasa.r0503@gmail.com>
2023-05-15 16:15:51 +0530

    added input of different types

--- API/src/main.rs
+++ API/src/main.rs
@@ -1,6 +1,89 @@
+trait OInput<T>
+{
+    fn o_take_inp_1(&self, t: T);
+}
+
+struct Input;
+impl Input
+{
+    fn take_inp_1<T>(&self, t: T) where Self: OInput<T>
+    {
+        self.o_take_inp_1(t);
+    }
+}
+
+impl OInput<&mut u64> for Input
+{
+    fn o_take_inp_1(&self, t: &mut u64)
+    {
+        let mut inp: u64 = 0;
+        let mut num: u64;
+        let mut c: u8 = 0;
+        loop {
+            syscalls::sys_read(0, &mut c, 1);
+            if c == 0 {
+                break;
+            }
+            if c == 10 {
+                break;
+            }
+            if c >= 48 && c <= 57 {
+                num = (c - 48).into();
+                inp = inp * 10 + num;
+            } else {
+                //panic if there is a non-number character
+                panic!("Invalid input");
+            }
+        }
+        *t = inp;
+    }
+}
+
+impl OInput<&mut i64> for Input
+{
+    fn o_take_inp_1(&self, t: &mut i64)
+    {
+        let mut inp: i64 = 0;
+        let mut num: i64;
+        let mut c: u8 = 0;
+        let mut neg: bool = false;
+        loop {
+            syscalls::sys_read(0, &mut c, 1);
+            if c == 0 {
+                break;
+            }
+            if c == 10 {
+                break;
+            }
+            if!neg && c == 45 {
+                neg = true;
+                continue;
+            }
+            if c >= 48 && c <= 57 {
+                num = (c - 48).into();
+                inp = inp * 10 + num;
+            } else {
+                //panic if there is a non-number character
+                panic!("Invalid input");
+            }
+        }
+        if neg {
+            *t = -inp;
+        }
+        else {
+            *t = inp;
+        }
+    }
+}
+
+macro_rules! input {
+    ($arg:expr) => {
+        Input.take_inp_1($arg);
+    };
+}
 
 fn main() {
-    let string = "Hello, world!\n";
-    syscalls::sys_fork();
-    syscalls::sys_write(1, string.as_ptr(), string.len() as u64);
+    let mut inp: i64 = 0;
+    input!(&mut inp);
+    println!("{}", inp);
 }
--- Syscalls/src/lib.rs
+++ Syscalls/src/lib.rs
@@ -1,6 +1,4 @@
 use std::arch::asm;
-use std::ffi::CStr;
-use std::os::raw::c_char;
 
 #[repr(u64)]
 pub enum LinuxSysCalls {
@@ -10,21 +8,6 @@ pub enum LinuxSysCalls {
     Close = 3,
     WriteV = 20,
     Fork = 57,
-}
-
-#[repr(u32)]
-#[allow(non_camel_case_types, unused)]
-pub enum LinuxFileFlags {
-    /// Open for reading only.
-    O_RDONLY = 0o0,
-    /// Open for writing only.
-    O_WRONLY = 0o1,
-    /// Opens a file for reading and writing.
-    O_RDWR = 0o2,
-    /// Create file if it doesn't exist.
-    O_CREAT = 0o100,
-    /// Append if file has content.
-    O_APPEND = 0o2000,
 }
 
 pub unsafe fn syscall(num: u64, arg1: u64, arg2: u64, arg3: u64) -> i64 {
@@ -57,36 +40,6 @@ pub fn sys_open(path: *const u8, flags: u32, umode: u16) -> i64 {
             umode as u64,
         )
     }
-}
-
-pub fn sys_writev(fd: u64, iovec: *const u8, vlen: u64) -> i64 {
-    unsafe { syscall(LinuxSysCalls::WriteV as u64, fd, iovec as u64, vlen) }
-}
-
-pub fn writev<const N: usize>(fd: u64, msgs: &[&CStr]) -> i64 {
-    #[derive(Copy, Clone)]
-    #[repr(C)]
-    struct iovec {
-        iov_base: *const c_char,
-        len: usize,
-    }
-    impl Default for iovec {
-        fn default() -> Self {
-            Self {
-                iov_base: std::ptr::null(),
-                len: 0,
-            }
-        }
-    }
-
-    let mut vector: [iovec; N] = [iovec::default(); N];
-    // copy the C-string pointers into the iovec-array
-    for (i, cstr) in msgs.iter().enumerate() {
-        vector[i].iov_base = cstr.as_ptr();
-        vector[i].len = cstr.to_bytes().len()
-    }
-    // execute the syscall
-    sys_writev(fd, vector.as_ptr() as *const u8, msgs.len() as u64)
 }
 
 pub fn sys_close(fd: u64) -> i64 {
