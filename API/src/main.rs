1b0ce1844ba0ad832086f02bec682343a73f4230
Srinivasa R <srinivasa.r0503@gmail.com>
2023-05-23 03:04:03 +0530

    added threads

--- API/src/main.rs
+++ API/src/main.rs
@@ -1,3 +1,7 @@
+use libc;
+use std::arch::asm;
+
+//INPUT
 trait OInput1<T>
 {
     fn o_take_inp_1(&self, t: T);
@@ -19,6 +23,16 @@ impl Input
     fn take_inp_2<T, U, V>(&self, t: T, u: U, v: V) where Self: OInput2<T, U, V>
     {
         self.o_take_inp_2(t, u, v);
+    }
+}
+
+impl OInput1<&mut char> for Input
+{
+    fn o_take_inp_1(&self, t: &mut char)
+    {
+        let mut c: u8 = 0;
+        syscalls::sys_read(0, &mut c, 1);
+        *t = c as char;
     }
 }
 
@@ -117,12 +131,170 @@ macro_rules! input {
         Input.take_inp_2($arg1, $arg2, $arg3);
     }
 }
+//end INPUT
+
+
+//PROCESS MANAGEMENT
+
+static mut PIDS: Vec<u64> = Vec::new();
+
+//forks
+fn fork() -> i64
+{
+    //create static variable to store the pids of the children
+    unsafe
+    {
+        let pid: i64 = syscalls::sys_fork();
+        if pid == 0 {
+            //child
+            return 0;
+        }
+        else if pid > 0 {
+            //parent
+            PIDS.push(pid as u64);
+            return pid;
+        }
+        return pid;
+    }
+}
+
+trait JoinAll
+{
+    fn join_all_i(&self);
+}
+
+trait JoinOne
+{
+    fn join_i(&self, pid: u64);
+}
+
+struct Join;
+impl Join
+{
+    fn join_all(&self) where Self: JoinAll
+    {
+        self.join_all_i();
+    }
+
+    fn join(&self, pid: u64) where Self: JoinOne
+    {
+        self.join_i(pid);
+    }
+}
+
+impl JoinAll for Join
+{
+    fn join_all_i(&self)
+    {
+        unsafe
+        {
+            let mut pid: u64;
+            let mut status: u64 = 0;
+            let mut i: usize = 0;
+            let mut len: usize;
+            loop {
+                len = PIDS.len();
+                if i >= len {
+                    break;
+                }
+                pid = PIDS[i];
+                let status_ptr: *mut u64 = &mut status;
+                syscalls::sys_wait4(pid, status_ptr, 0, 0 as *mut u64);
+                i = i + 1;
+            }
+        }
+    }
+}
+
+impl JoinOne for Join
+{
+    fn join_i(&self, pid: u64)
+    {
+        let mut status: u64 = 0;
+        let status_ptr: *mut u64 = &mut status;
+        //check if pid is in PIDS
+        // unsafe
+        // {
+        //     let mut i: usize = 0;
+        //     let mut len: usize;
+        //     loop {
+        //         len = PIDS.len();
+        //         if i >= len {
+        //             break;
+        //         }
+        //         if PIDS[i] == pid {
+        //             break;
+        //         }
+        //         i = i + 1;
+        //     }
+        // }
+        syscalls::sys_wait4(pid, status_ptr, 0, 0 as *mut u64);
+    }
+}
+
+macro_rules! join {
+    () => {
+        Join.join_all();
+    };
+
+    ($arg:expr) => {
+        Join.join($arg);
+    }
+}
+
+unsafe fn exit_thread()
+{
+    asm!{
+        "pop rax",
+        "sub rsp, 4096 * 1024 + 1024",
+        "mov rsi, 4096 * 1024",
+        "mov rdi, rsp",
+        "mov rax, 11",
+        "syscall",
+        "mov rax, 60",
+        "syscall",
+    }
+    return;
+}
+
+unsafe fn new_thread(ptr: fn() -> ()) -> u64
+{
+
+    let stack_ptr: u64 = syscalls::stack_create();
+    let exit: unsafe fn() -> () = exit_thread;
+    let flags: u64 = (libc::CLONE_VM | libc::CLONE_FS | libc::CLONE_FILES | libc::CLONE_SIGHAND | libc::CLONE_PARENT |libc::CLONE_THREAD | libc::CLONE_IO) as u64;
+    asm!{
+        "push r14",
+        "push rax",
+        "pop qword ptr [rdi + 8]",
+        "pop qword ptr [rdi]",
+        in("rdi") stack_ptr,
+        in("rax") exit,
+        in("r14") ptr,
+    };
+    let tid: u64;
+    asm!{
+        "xor r8, r8",
+        "xor r10, r10",
+        "xor rdx, rdx",
+        "syscall",
+        in("rdi") flags,
+        in("rsi") stack_ptr,
+        in("rax") 56,
+        lateout("r10") tid,
+    };
+    return tid;
+}
+
+fn test_thread()
+{
+    print!("Hello from thread\n");
+    return;
+}
 
 fn main() {
-    let mut inp: i64 = 0;
-    let mut line: String = "".to_string();
-    input!(&mut inp);
-    input!(&mut line, 100, '\n');
-    println!("{}", inp);
-    println!("{}", line);
+    let pid: u64;
+    unsafe { pid = new_thread(test_thread); }
+    print!("Hello from main\n");
+    join!(pid);
 }
--- Syscalls/src/lib.rs
+++ Syscalls/src/lib.rs
@@ -1,4 +1,5 @@
 use std::arch::asm;
+use libc;
 
 #[repr(u64)]
 pub enum LinuxSysCalls {
@@ -8,6 +9,10 @@ pub enum LinuxSysCalls {
     Close = 3,
     WriteV = 20,
     Fork = 57,
+    Execve = 59,
+    Wait4 = 61,
+    Clone = 56,
+    Exit = 60,
 }
 
 pub unsafe fn syscall(num: u64, arg1: u64, arg2: u64, arg3: u64) -> i64 {
@@ -48,4 +53,45 @@ pub fn sys_close(fd: u64) -> i64 {
 
 pub fn sys_fork() -> i64 {
     unsafe { syscall(LinuxSysCalls::Fork as u64, 0, 0, 0) }
+}
+
+pub fn sys_execve(path: *const u8, argv: *const *const u8, envp: *const *const u8) -> i64 {
+    unsafe { syscall(LinuxSysCalls::Execve as u64, path as u64, argv as u64, envp as u64) }
+}
+
+pub fn sys_wait4(pid: u64, status: *mut u64, options: u32, rusage: *mut u64) -> i64 {
+    unsafe { syscall(LinuxSysCalls::Wait4 as u64, pid, status as u64, options as u64) }
+}
+
+pub fn sys_clone(flags: u64, stack: u64) -> i64 {
+    unsafe { syscall(LinuxSysCalls::Clone as u64, flags, stack, 0) }
+}
+
+pub fn sys_exit(status: u64) -> i64 {
+    unsafe { syscall(LinuxSysCalls::Exit as u64, status, 0, 0) }
+}
+
+//USEFUL FUNCTIONS FOR THREADS
+
+pub unsafe fn stack_create() -> u64 {
+    let mut stack: u64;
+    let stack_size: u64 = 4096 * 1024;
+    asm!{
+        "xor r9, r9",
+        "xor rdi, rdi",
+        "mov rax, 9",
+        "syscall",
+        "mov rdx, 0",
+        "mov rsi, 1024",
+        "mov rdi, rax",
+        "mov rax, 10",
+        "syscall",
+        "add rdi, 4096 * 1024 + 1024 - 16",
+        in("rsi") stack_size + 1024,
+        in("rdx") libc::PROT_READ | libc::PROT_WRITE,
+        in("r10") libc::MAP_PRIVATE | libc::MAP_ANONYMOUS,
+        in("r8") -1,
+        lateout("rdi") stack,
+    };
+    stack
 }
